9511831017c853ed8a81804debddc3ef8c8ef2df
Bodyes <107073845+Bodyes@users.noreply.github.com>
2022-06-08 20:42:50 +0200

    Add files via upload

---
+++ C/sortint.h
@@ -0,0 +1,9 @@
+#ifndef ZAD18_SORTINT_H
+#define ZAD18_SORTINT_H
+
+void bubbleSort(int*, int, int, int (const void *, const void *));
+void selectSort(int*, int, int, int (const void *, const void *));
+int comp_int(const void*, const void*);
+void swap(void *, void *, int);
+
+#endif
