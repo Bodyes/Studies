baf5d7da3251f7d14be48cb29ba711468327bebc
Bodyes <107073845+Bodyes@users.noreply.github.com>
2022-06-08 20:38:35 +0200

    Delete C directory

--- C/m_get.c
+++
@@ -1,6 +0,0 @@
-#include "matrix.h"
-
-int m_get(int x, int y, int newX, int newY, MATRIX macierz) {
-    int temp = y * (newX - 1) + newY - 1;
-    return macierz.wsk[temp];
-}
--- C/m_put.c
+++
@@ -1,7 +0,0 @@
-#include "matrix.h"
-int m_put(int x, int y, int newX, int newY, int value, MATRIX macierz) {
-    int temp = y * (newX - 1) + newY - 1;
-
-    macierz.wsk[temp] = value;
-    return 1;
-}
