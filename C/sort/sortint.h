3db5faca7f79f023fd02d5c6ab088a7fdf9a1ef4
Bodyes <107073845+Bodyes@users.noreply.github.com>
2022-06-08 20:47:18 +0200

    Delete C/sort directory

--- C/sort/sortint.h
+++
@@ -1,9 +0,0 @@
-#ifndef ZAD18_SORTINT_H
-#define ZAD18_SORTINT_H
-
-void bubbleSort(int*, int, int, int (const void *, const void *));
-void selectSort(int*, int, int, int (const void *, const void *));
-int comp_int(const void*, const void*);
-void swap(void *, void *, int);
-
-#endif
